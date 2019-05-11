a153a56acdbe3fa788688b68b62f90747c6c8f7d
Wanaoi <wanao2008@hotmail.com>
2019-05-11 14:43:19 +0800

    Frist commit

--- include/main.hpp
+++ include/main.hpp
@@ -1,4 +1,4 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-void print_ok(char *msg);
+void print_ok(const char *msg);
 #endif//_MAIN_H_
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,22 @@
 #include "main.hpp"
 #include "libdemo.hpp"
 using namespace std;
+/**
+ * [main description]
+ * @param  argc [description]
+ * @param  argv [description]
+ * @return      [description]
+ */
 int main(int argc, const char** argv)
 {
 	print_ok("Hello!");
 	libdemo_output();
     return 0;
 }
-void print_ok(char *msg){
+/**
+ * [print_ok description]
+ * @param msg [description]
+ */
+void print_ok(const char *msg){
 	cout << msg << endl;
 }
